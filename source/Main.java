d6c26d41f0aed298b1a3f81604c685be8d1786b0
KaminarisKonstantinos <kaminariskonstantinos@gmail.com>
2021-06-08 21:41:04 +0300

    prosthikhkartassthnlista()

--- source/Main.java
+++ source/Main.java
@@ -310,110 +310,6 @@ public class Main extends Application
          window.show();
      }
 
-     public void editstock(int x){
-         VBox mypane = new VBox();
-         mypane.setPadding(new Insets(10));
-         mypane.setSpacing(8);
-         mypane.setAlignment(Pos.CENTER);
-         ScrollPane scrollPane = new ScrollPane(mypane);
-         scrollPane.setFitToHeight(true);
-         BorderPane miniborder = new BorderPane(scrollPane);
-         BorderPane border = new BorderPane();
-         Text title = new Text("CATEGORIES");
-         title.setFont(Font.font("Arial", FontWeight.BOLD, 14));
-         mypane.getChildren().add(title);
-         Button[] array = new Button[stores.get(x).getcategories().size()];
-         if(stores.get(x).getcategories().size()==0){mypane.getChildren().add(new Label("NO CATEGORIES ADDED"));}
-         else{
-             for (int i = 0; i < stores.get(x).getcategories().size(); i++) {
-                 array[i] = new Button(stores.get(x).getcategories().get(i));
-                 mypane.getChildren().add(array[i]);
-             }
-         }
-         for (int i = 0; i < stores.get(x).getcategories().size(); i++) {
-             int finalI = i;
-             array[i].setOnAction(e -> editcategory(x, finalI));
-         }
-         Button aButton = new Button("ADD CATEGORY");
-         mypane.getChildren().add(aButton);
-         GridPane.setHalignment(aButton, HPos.LEFT);
-         aButton.setOnAction(e -> addcategory(x));
-
-         VBox mypane5 = new VBox();
-         mypane5.setPadding(new Insets(10));
-         mypane5.setSpacing(8);
-         mypane5.setAlignment(Pos.CENTER);
-         Text title4 = new Text("OFFERS");
-         title4.setFont(Font.font("Arial", FontWeight.BOLD, 14));
-         mypane5.getChildren().add(title4);
-         Button[] array3 = new Button[stores.get(x).getoffers().size()];
-         if(stores.get(x).getoffers().size()==0){mypane5.getChildren().add(new Label("NO OFFERS ADDED"));}
-         else{
-             for (int i = 0; i < stores.get(x).getoffers().size(); i++) {
-                 array3[i] = new Button(stores.get(x).getoffers().get(i).getoffername());
-                 mypane5.getChildren().add(array3[i]);
-             }
-         }
-         for (int i = 0; i < stores.get(x).getoffers().size(); i++) {
-             int finalI = i;
-             array3[i].setOnAction(e ->editoffer(x, finalI));
-         }
-         Button oButton = new Button("ADD OFFER");
-         mypane5.getChildren().add(oButton);
-         GridPane.setHalignment(oButton, HPos.LEFT);
-         oButton.setOnAction(e -> {
-             Offer temp = new Offer();
-             addoffer(x,temp,-1);
-         });
-
-
-         VBox mypane2 = new VBox();
-         mypane2.setPadding(new Insets(10));
-         mypane2.setSpacing(8);
-         mypane2.setAlignment(Pos.CENTER);
-         Text title2 = new Text("PRODUCTS");
-         title2.setFont(Font.font("Arial", FontWeight.BOLD, 14));
-         mypane2.getChildren().add(title2);
-         Button[] array2 = new Button[stores.get(x).getproducts().size()];
-         if(stores.get(x).getproducts().size()==0){mypane2.getChildren().add(new Label("NO PRODUCTS ADDED"));}
-         else{
-             for (int i = 0; i < stores.get(x).getproducts().size(); i++) {
-                 array2[i] = new Button(stores.get(x).getproducts().get(i).getname());
-                 mypane2.getChildren().add(array2[i]);
-             }
-             for (int i = 0; i < stores.get(x).getproducts().size(); i++) {
-                 int finalI = i;
-                 array2[i].setOnAction(e -> editproduct(x, finalI));
-             }
-         }
-         Button aButton2 = new Button("ADD PRODUCT");
-         mypane2.getChildren().add(aButton2);
-         GridPane.setHalignment(aButton2, HPos.CENTER);
-         aButton2.setOnAction(e -> addproduct(x));
-         VBox mypane4 = new VBox();
-         mypane4.setPadding(new Insets(10));
-         mypane4.setSpacing(8);
-         mypane4.setAlignment(Pos.BOTTOM_RIGHT);
-         Button aButton3 = new Button("BACK");
-         mypane4.getChildren().add(aButton3);
-         aButton3.setOnAction(e -> arxikhselidastore(x));
-
-         miniborder.setRight(mypane5);
-         miniborder.setLeft(mypane);
-         miniborder.setCenter(mypane2);
-         border.setCenter(miniborder);
-         border.setRight(mypane4);
-         Text title3 = new Text("BOS");
-         title3.setFill(Color.GREEN);
-         title3.setFont(Font.font ("Verdana", 20));
-         VBox mypane3 = new VBox();
-         mypane3.setAlignment(Pos.CENTER);
-         mypane3.getChildren().add(title3);
-         border.setTop(mypane3);
-         window.setScene(new Scene(border, 800, 700));
-         window.show();
-     }
-
     public void anazhthshkatasthmatos(int y,int mod,String b){ //opou y thesh tou pelath sto arraylist, mod=mode opou -1 den exoume filtro kathgorias alliws h kathgoria -2 search bar
          int mode;
          mode=mod;
@@ -1171,6 +1067,59 @@ public class Main extends Application
          border.setCenter(mypane);
          border.setBottom(mypane2);
          window.setScene(new Scene(border, 800, 700));
+         window.show();
+     }
+
+     public void prosthikhkartassthnlista(int x,int y,Order temp){// y = customer , x = sotre
+         GridPane mypane = new GridPane();
+         mypane.setPadding(new Insets(10));
+         mypane.setHgap(5);
+         mypane.setVgap(3);
+         mypane.setAlignment(Pos.CENTER);
+         TextField nametf = new TextField();
+         TextField cardtf = new TextField();
+         TextField datetf = new TextField();
+         TextField cvvtf = new TextField();
+         mypane.add(nametf, 1, 1);
+         mypane.add(cardtf, 1, 2);
+         mypane.add(datetf, 1, 3);
+         mypane.add(cvvtf, 1, 4);
+         mypane.add(new Label("NAME:"), 0, 1);
+         mypane.add(new Label("CARD:"), 0, 2);
+         mypane.add(new Label("DATE:"), 0, 3);
+         mypane.add(new Label("CVV:"), 0, 4);
+         Button confirmButton = new Button("READY");
+         mypane.add(confirmButton, 1, 5);
+         GridPane.setHalignment(confirmButton, HPos.LEFT);
+         confirmButton.setOnAction(e -> {
+             Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+             alert.setTitle("BOS");
+             alert.setHeaderText(null);
+             alert.setContentText("το σύστημα επικοινωνεί με την τράπεζα και αυτη:");
+             ButtonType buttona = new ButtonType("επικυρώνει την κάρτα");
+             ButtonType buttonb= new ButtonType("απορρίπτει κάρτα");
+             alert.getButtonTypes().setAll(buttona, buttonb);
+             Optional<ButtonType> result = alert.showAndWait();
+             if (result.get() == buttona){
+                 temp.setkarta(new Card(nametf.getText(),cardtf.getText(),datetf.getText(),cvvtf.getText()));
+                 ArrayList<Card> cards2 =customers.get(y).getcards();
+                 cards2.add(temp.getkarta());
+                 customers.get(y).setcards(cards2);
+                 completeorder(x,y,2);
+             } else {
+                 Alert alert2 = new Alert(Alert.AlertType.ERROR);
+                 alert2.setTitle("BOS");
+                 alert2.setHeaderText(null);
+                 alert2.setContentText("CARD NOT ACCEPTED");
+                 alert2.showAndWait();
+                 completeorder(x, y, 0);
+             }
+         });
+         Button bButton = new Button("BACK");
+         mypane.add(bButton, 0, 5);
+         GridPane.setHalignment(bButton, HPos.LEFT);
+         bButton.setOnAction(e -> completeorder(x,y,0));
+         window.setScene(new Scene(mypane, 800, 700));
          window.show();
      }
 
